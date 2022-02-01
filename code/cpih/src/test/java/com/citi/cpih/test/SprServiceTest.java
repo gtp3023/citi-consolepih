797f2d365a625400985c888cc297e99dce3d2c52
Marcos Eliud Martinez Rodriguez <mmartinezr@citivirt.citi.com.mx>
2022-02-01 11:37:31 -0600

    TA #64484 : pruebas unitarias fix

--- code/cpih/src/test/java/com/citi/cpih/test/SprServiceTest.java
+++ code/cpih/src/test/java/com/citi/cpih/test/SprServiceTest.java
@@ -6,16 +6,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.citi.cpih.ServerApplication;
 import com.citi.cpih.config.TestConfig;
 import com.citi.cpih.dto.UserSprDTO;
-import com.citi.cpih.service.SprService;
 import com.citi.cpih.service.impl.SprServiceImpl;
 
 @ComponentScan(basePackages = "com.citi.cpih")
