d655657cfd69c797ec8f9f79cf3bfb08ac0615a5
Marcos Eliud Martinez Rodriguez <mmartinezr@citivirt.citi.com.mx>
2022-02-01 11:36:13 -0600

    TA #64484 : pruebas unitarias

---
+++ code/cpih/src/test/java/com/citi/cpih/test/SprServiceTest.java
@@ -0,0 +1,45 @@
+package com.citi.cpih.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.citi.cpih.ServerApplication;
+import com.citi.cpih.config.TestConfig;
+import com.citi.cpih.dto.UserSprDTO;
+import com.citi.cpih.service.SprService;
+import com.citi.cpih.service.impl.SprServiceImpl;
+
+@ComponentScan(basePackages = "com.citi.cpih")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = TestConfig.class)
+public class SprServiceTest {
+	private final Logger logger = LogManager.getLogger(this.getClass().getName());
+
+	private SprServiceImpl sprService;
+
+	@Test
+	@SuppressWarnings("unused")
+	public void getProfileSpr() {
+		try {
+			sprService = new SprServiceImpl();
+			String msisdn = "5554004495";
+			
+			UserSprDTO user = sprService.getProfile(msisdn);
+
+			assertNotNull(sprService);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+	}
+
+}
